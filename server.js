9aab8bd7d098e1248741055d4aa082ae8432a715
keshavshriram <keshavshriram2002@gmail.com>
2024-05-24 17:21:34 +0530

    updated server file

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// server.js
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+// Read db.json file into memory
+const dbFilePath = path.join(__dirname, 'db.json');
+const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'));
+
+const router = jsonServer.router(dbData);
+
+server.use(middlewares);
+server.use(router);
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
+
